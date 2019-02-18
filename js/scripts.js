cb7c8673ebb807078f2ae9ea7214959078f983c1
iettle <42851851+iettle@users.noreply.github.com>
2019-02-17 21:54:42 -0500

    Initial commit.
    Nav bar. About.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+
+  /**********************************************************
+   *                        LOADING                         *
+   **********************************************************/
+  $("#loadingbg").delay(2300).fadeOut("slow");
+  $("#load").delay(2000).fadeOut("slow");
+
+  /**********************************************************
+   *                     INITIAL SCREEN                     *
+   **********************************************************/
+  // Array that will be looped through.
+  var des = [
+    'student',
+    'computer scientist',
+    'linguist',
+    'artist',
+  ];
+
+  // This function repeatedly loops through the des array.
+  var index = -1;
+  $(function loop() {
+    index++;
+    $("#changetext").fadeOut(1500, function() {
+       $(this).text(des[index]).fadeIn(1500);
+       index = (index == des.length-1) ? -1 : index;
+       loop();
+     });
+   });
+});
